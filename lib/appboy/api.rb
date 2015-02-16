0a29836fa0e109e73a88cf3aff528a372f44a105
Hugo Bastien <hugobast@gmail.com>
2015-02-15 19:45:49 -0500

    Removes redundant json heads and to_json

--- lib/appboy/api.rb
+++ lib/appboy/api.rb
@@ -10,38 +10,35 @@ module Appboy
 
     def track_users(attributes: [], events: [], purchases: [])
       connection.post '/users/track' do |request|
-        request.headers['Content-Type'] = 'application/json'
         request.body = {
           app_group_id:   app_group_id,
           attributes:     attributes,
           events:         events,
           purchases:      purchases
-        }.to_json
+        }
       end
     end
 
     def send_messages(messages:, user_ids:, segment_id: nil)
       connection.post '/messages/send' do |request|
-        request.headers['Content-Type'] = 'application/json'
         request.body = {
           app_group_id:      app_group_id,
           messages:          messages,
           external_user_ids: user_ids,
           segment_ids:       [segment_id].compact
-        }.to_json
+        }
       end
     end
 
     def schedule_messages(send_at:, messages:, segment_id:, local_timezone: false)
       connection.post '/messages/schedule' do |request|
-        request.headers['Content-Type'] = 'application/json'
         request.body = {
           app_group_id:      app_group_id,
           segment_ids:               [segment_id],
           send_at:                   send_at,
           deliver_in_local_timezone: local_timezone,
           messages:                  messages
-        }.to_json
+        }
       end
     end
 
