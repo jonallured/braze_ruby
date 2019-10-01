91835f38ac83d4c46680c51582a0b05201ad07ce
Wojciech Wnętrzak <w.wnetrzak@gmail.com>
2019-10-01 13:40:41 +0200

    Added canvas details API

--- lib/braze_ruby/endpoints/canvas.rb
+++ lib/braze_ruby/endpoints/canvas.rb
@@ -4,6 +4,10 @@ module BrazeRuby
       def trigger_canvas_send(**payload)
         BrazeRuby::REST::TriggerCanvasSend.new(api_key, braze_url, payload).perform
       end
+
+      def canvas_details(**payload)
+        BrazeRuby::REST::CanvasDetails.new(api_key, braze_url, payload).perform
+      end
     end
   end
 end
---
+++ lib/braze_ruby/rest/canvas_details.rb
@@ -0,0 +1,20 @@
+module BrazeRuby
+  module REST
+    class CanvasDetails < Base
+      attr_reader :api_key, :params
+
+      def initialize(api_key, braze_url, **params)
+        @api_key = api_key
+        @params = params
+        super braze_url
+      end
+
+      def perform
+        http.get('/canvas/details', {
+          'api_key': api_key,
+          **@params
+        })
+      end
+    end
+  end
+end
