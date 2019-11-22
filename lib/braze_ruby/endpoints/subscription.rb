cc06fe66965a9a78896c50662f411a85e8b6f06c
Tales Cione <talesj@gmail.com>
2019-11-22 09:00:27 +0100

    Add api call to subscription user status

--- lib/braze_ruby/endpoints/subscription.rb
+++ lib/braze_ruby/endpoints/subscription.rb
@@ -8,6 +8,11 @@ module BrazeRuby
       def subscription_status_get(**payload)
         BrazeRuby::REST::SubscriptionStatusGet.new(api_key, braze_url, payload).perform
       end
+
+
+      def subscription_user_status(**payload)
+        BrazeRuby::REST::SubscriptionUserStatus.new(api_key, braze_url, payload).perform
+      end
     end
   end
 end
--- spec/integrations/subscription_spec.rb
+++ spec/integrations/subscription_spec.rb
@@ -1,22 +1,12 @@
 require 'spec_helper'
-require 'pry'
 
 RSpec.describe 'subscription', vcr: true do
-  let(:subscription_group_id) do
-    '0201b87a-ef6e-4410-9412-a298c4c1206d'
-  end
+  let(:subscription_group_id) { '0201b87a-ef6e-4410-9412-a298c4c1206d' }
   let(:subscribed_user) { 'test-gem-001' }
   let(:unsubscribed_user) { 'test-gem-002' }
   let(:never_subscribed_user) { 'test-gem-003' }
 
-  describe 'subscription_status_get' do
-    subject(:response) do
-      api.subscription_status_get(
-        external_id: external_id,
-        subscription_group_id: subscription_group_id
-      )
-    end
-
+  context 'when getting subscription group statuses' do
     let(:json_response) { JSON.parse(response.body) }
 
     before do
@@ -33,30 +23,82 @@ RSpec.describe 'subscription', vcr: true do
       )
     end
 
-    context 'when checking subscribed and unsubscribed user status' do
+    describe 'subscription_user_status' do
+      subject(:response) do
+        api.subscription_user_status(
+          external_id: external_id,
+        )
+      end
+
       let(:external_id) { [subscribed_user, unsubscribed_user, never_subscribed_user] }
 
-      it 'returns only users that were once subscribed' do
+      it 'returns subscription groups and status for every group' do
         expect(response.status).to eq(200)
-        expect(json_response).to eq(
+        expect(json_response).to match(
           'message' => 'success',
-          'status' => {
-            'test-gem-001' => 'Subscribed',
-            'test-gem-002' => 'Unsubscribed',
-            'test-gem-003' => 'unknown'
-          }
+          'total_count' => 3,
+          'users' => [
+            hash_including(
+              'external_id' => subscribed_user,
+              'subscription_groups' => [
+                hash_including(
+                  'id' => subscription_group_id,
+                  'status' => 'Subscribed'
+                )
+              ]
+            ),
+            hash_including(
+              'external_id' => unsubscribed_user,
+              'subscription_groups' => [
+                hash_including(
+                  'id' => subscription_group_id,
+                  'status' => 'Unsubscribed'
+                )
+              ]
+            ),
+            hash_including(
+              'external_id' => never_subscribed_user,
+              'subscription_groups' => []
+            )
+          ]
         )
       end
     end
 
-    context 'when group does not exist' do
-      let(:external_id) { subscribed_user }
-      let(:subscription_group_id) { SecureRandom.uuid }
+    describe 'subscription_status_get' do
+      subject(:response) do
+        api.subscription_status_get(
+          external_id: external_id,
+          subscription_group_id: subscription_group_id
+        )
+      end
 
-      it 'returns an error' do
-        expect(response.status).to eq(400)
+      context 'when checking subscribed and unsubscribed user status' do
+        let(:external_id) { [subscribed_user, unsubscribed_user, never_subscribed_user] }
+
+        it 'returns only users that were once subscribed' do
+          expect(response.status).to eq(200)
+          expect(json_response).to eq(
+            'message' => 'success',
+            'status' => {
+              'test-gem-001' => 'Subscribed',
+              'test-gem-002' => 'Unsubscribed',
+              'test-gem-003' => 'unknown'
+            }
+          )
+        end
+      end
+
+      context 'when group does not exist' do
+        let(:external_id) { subscribed_user }
+        let(:subscription_group_id) { 'non-existing-subscription-group' }
+
+        it 'returns an error' do
+          expect(response.status).to eq(400)
+        end
       end
     end
+
   end
 
   describe 'subscription_status_set' do
@@ -95,7 +137,7 @@ RSpec.describe 'subscription', vcr: true do
     end
 
     context 'when subscription group does not exist' do
-      let(:subscription_group_id) { SecureRandom.uuid }
+      let(:subscription_group_id) { 'non-existing-subscription-group' }
 
       it 'returns an error status' do
         expect(response.status).to eq(400)
