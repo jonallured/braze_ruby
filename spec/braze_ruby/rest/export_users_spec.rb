0b705d1d35e5a27380886ee3987bf3e4d20e16ac
Jon Allured <jon.allured@gmail.com>
2021-07-21 09:45:49 -0500

    Rewrite specs to cover more behavior

--- spec/braze_ruby/rest/export_users_spec.rb
+++ spec/braze_ruby/rest/export_users_spec.rb
@@ -5,17 +5,52 @@ require "spec_helper"
 describe BrazeRuby::REST::ExportUsers do
   let(:http) { double(:http) }
 
-  let(:payload) { {external_ids: external_ids} }
-  let(:external_ids) { [1] }
+  context "with external ids" do
+    let(:payload) { {external_ids: [1]} }
 
-  subject { described_class.new :api_key, :rest_url, {} }
+    it "makes an api call to the users export by id endpoint" do
+      expect(http).to receive(:post).with("/users/export/ids", payload)
 
-  before { subject.http = http }
+      export_users = BrazeRuby::REST::ExportUsers.new(:api_key, :rest_url, {})
+      export_users.http = http
+      export_users.perform(**payload)
+    end
+  end
 
-  it "makes an http call to the track user endpoint" do
-    expect(http).to receive(:post).with "/users/export/ids",
-      payload
+  context "with a segment id" do
+    let(:payload) { {segment_id: 1} }
 
-    subject.perform(**payload)
+    it "makes an api call to the users export by segment endpoint" do
+      expect(http).to receive(:post).with("/users/export/segment", payload)
+
+      export_users = BrazeRuby::REST::ExportUsers.new(:api_key, :rest_url, {})
+      export_users.http = http
+      export_users.perform(**payload)
+    end
+  end
+
+  context "without any ids" do
+    let(:payload) { {} }
+
+    it "does not make an api call" do
+      expect(http).to_not receive(:post)
+
+      export_users = BrazeRuby::REST::ExportUsers.new(:api_key, :rest_url, {})
+      export_users.http = http
+      export_users.perform(**payload)
+    end
+  end
+
+  context "with both types of ids" do
+    let(:payload) { {external_ids: [1], segment_id: 1} }
+
+    it "prefers the export by id endpoint and ignores the segment id" do
+      expect(http).to receive(:post).with("/users/export/ids", {external_ids: [1]})
+      expect(http).to_not receive(:post).with("/users/export/segment", anything)
+
+      export_users = BrazeRuby::REST::ExportUsers.new(:api_key, :rest_url, {})
+      export_users.http = http
+      export_users.perform(**payload)
+    end
   end
 end
