68f90bf6fdd5e9c9fa07accdfc867a4db7406f46
Jon Allured <jon.allured@gmail.com>
2021-07-21 09:51:04 -0500

    Make test cases more declarative

--- spec/braze_ruby/rest/export_users_spec.rb
+++ spec/braze_ruby/rest/export_users_spec.rb
@@ -5,44 +5,47 @@ require "spec_helper"
 describe BrazeRuby::REST::ExportUsers do
   let(:http) { double(:http) }
 
+  let(:external_ids) { nil }
+  let(:segment_id) { nil }
+  let(:options) { {} }
+
   context "with external ids" do
-    let(:payload) { {external_ids: [1]} }
+    let(:external_ids) { [1] }
 
     it "makes an api call to the users export by id endpoint" do
-      expect(http).to receive(:post).with("/users/export/ids", payload)
+      expect(http).to receive(:post).with("/users/export/ids", external_ids: external_ids)
 
       export_users = BrazeRuby::REST::ExportUsers.new(:api_key, :rest_url, {})
       export_users.http = http
-      export_users.perform(**payload)
+      export_users.perform(external_ids: external_ids, segment_id: segment_id, **options)
     end
   end
 
   context "with a segment id" do
-    let(:payload) { {segment_id: 1} }
+    let(:segment_id) { 1 }
 
     it "makes an api call to the users export by segment endpoint" do
-      expect(http).to receive(:post).with("/users/export/segment", payload)
+      expect(http).to receive(:post).with("/users/export/segment", segment_id: 1)
 
       export_users = BrazeRuby::REST::ExportUsers.new(:api_key, :rest_url, {})
       export_users.http = http
-      export_users.perform(**payload)
+      export_users.perform(external_ids: external_ids, segment_id: segment_id, **options)
     end
   end
 
   context "without any ids" do
-    let(:payload) { {} }
-
     it "does not make an api call" do
       expect(http).to_not receive(:post)
 
       export_users = BrazeRuby::REST::ExportUsers.new(:api_key, :rest_url, {})
       export_users.http = http
-      export_users.perform(**payload)
+      export_users.perform(external_ids: external_ids, segment_id: segment_id, **options)
     end
   end
 
   context "with both types of ids" do
-    let(:payload) { {external_ids: [1], segment_id: 1} }
+    let(:external_ids) { [1] }
+    let(:segment_id) { 1 }
 
     it "prefers the export by id endpoint and ignores the segment id" do
       expect(http).to receive(:post).with("/users/export/ids", {external_ids: [1]})
@@ -50,7 +53,7 @@ describe BrazeRuby::REST::ExportUsers do
 
       export_users = BrazeRuby::REST::ExportUsers.new(:api_key, :rest_url, {})
       export_users.http = http
-      export_users.perform(**payload)
+      export_users.perform(external_ids: external_ids, segment_id: segment_id, **options)
     end
   end
 end
