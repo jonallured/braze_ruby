035d07c2b2a7be7a6f184312be59adf96afe6b59
Damian Galarza <galarza.d@gmail.com>
2021-12-23 15:09:14 -0500

    Add support for deleting scheduled triggers
    https://www.braze.com/docs/api/endpoints/messaging/schedule_messages/post_delete_scheduled_triggered_messages/

--- lib/braze_ruby/endpoints/campaigns.rb
+++ lib/braze_ruby/endpoints/campaigns.rb
@@ -3,6 +3,10 @@
 module BrazeRuby
   module Endpoints
     module Campaigns
+      def delete_campaign_trigger_schedule(**payload)
+        BrazeRuby::REST::DeleteTriggerCampaignSchedule.new(api_key, braze_url, options).perform(**payload)
+      end
+
       def trigger_campaign_send(**payload)
         BrazeRuby::REST::TriggerCampaignSend.new(api_key, braze_url, options, **payload).perform
       end
