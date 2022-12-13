d17d04c73a9a589168b620995ff41e757c146bd9
patsanch <patsanch@hey.com>
2022-12-13 11:54:17 -0800

    added better fix for export campaigns list spec

--- spec/braze_ruby/rest/export_campaigns_list_spec.rb
+++ spec/braze_ruby/rest/export_campaigns_list_spec.rb
@@ -4,13 +4,13 @@ require "spec_helper"
 
 describe BrazeRuby::REST::ExportCampaignsList do
   let(:http) { double(:http) }
-  let(:options) { {} }
+  let(:options) { {page: 1} }
 
   it "makes an api call to the export campaigns endpoint" do
     expect(http).to receive(:get).with("/campaigns/list", options)
 
     export_users = BrazeRuby::REST::ExportCampaignsList.new(:api_key, :rest_url, options)
     export_users.http = http
-    export_users.perform(options)
+    export_users.perform(**options)
   end
 end
