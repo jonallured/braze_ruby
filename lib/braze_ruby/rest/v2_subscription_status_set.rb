08b08e0538d93c77052f29b023cb9b77762cd5e8
Myles Cowper-Coles <mylescc@gmail.com>
2022-12-15 14:25:52 +0000

    Adding support for v2 subscription endpoint
    What:
    
    - Adding support for the v2 subscription group endpoint
    
    Why:
    
    - To ensure the gem is up to date.
    - Docs: https://www.braze.com/docs/api/endpoints/subscription_groups/post_update_user_subscription_group_status_v2/

---
+++ lib/braze_ruby/rest/v2_subscription_status_set.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+module BrazeRuby
+  module REST
+    class V2SubscriptionStatusSet < Base
+      attr_reader :params
+
+      def initialize(api_key, braze_url, options, **params)
+        @params = params
+        super api_key, braze_url, options
+      end
+
+      def perform
+        http.post("/v2/subscription/status/set", {
+          **params
+        })
+      end
+    end
+  end
+end
