b423cff56d5cc4cb71eafd19b29d9a872a30e049
Hugo Lepetit <hugo.lepetit@getaround.com>
2023-07-10 14:54:35 +0200

    :cop:

--- spec/braze_ruby/api_spec.rb
+++ spec/braze_ruby/api_spec.rb
@@ -5,8 +5,7 @@ require "spec_helper"
 describe BrazeRuby::API do
   let(:rest_api_key) { SecureRandom.uuid }
   let(:rest_url) { "https://braze.example.com" }
-  let(:options) { { option: "value" } }
-
+  let(:options) { {option: "value"} }
 
   context "takes into account config options for implicit instanciation" do
     subject(:api) { BrazeRuby::API.new }
@@ -19,32 +18,32 @@ describe BrazeRuby::API do
       end
     end
 
-    it 'braze_url' do
+    it "braze_url" do
       expect(api.braze_url).to eq(rest_url)
     end
 
-    it 'api_key' do
+    it "api_key" do
       expect(api.api_key).to eq(rest_api_key)
     end
 
-    it 'options' do
-      expect(api.options).to eq({ option: "value" })
+    it "options" do
+      expect(api.options).to eq({option: "value"})
     end
   end
 
   context "supports explicit instanciation" do
     subject(:api) { BrazeRuby::API.new(rest_api_key, rest_url, options) }
 
-    it 'braze_url' do
+    it "braze_url" do
       expect(api.braze_url).to eq(rest_url)
     end
 
-    it 'api_key' do
+    it "api_key" do
       expect(api.api_key).to eq(rest_api_key)
     end
 
-    it 'options' do
-      expect(api.options).to eq({ option: "value" })
+    it "options" do
+      expect(api.options).to eq({option: "value"})
     end
   end
 end
